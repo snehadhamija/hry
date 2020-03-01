a37950a143f35d2ae7d10b6c8978cc040fa44f5b
your name <you2example.com>
2020-03-01 11:49:59 +0530

    done

---
+++ gallery.php
@@ -0,0 +1,178 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<?php include("HEAD.PHP"); ?>
+<body>
+	<!-- header-section-starts -->
+  <?php include("top-nav.php"); ?> 
+	<div class="full">
+    <div class="col-md-3 top-nav gallery-sec">
+			<?php include("left-nav.php"); ?>
+	<div class="col-md-9 main">
+	  <!--grids-->
+	   <div class="gallery-section">
+	     <h3 class="tittle">Gallery <i class="glyphicon glyphicon-fullscreen"></i></h3>
+         <div class="categorie-grids cs-style-1">
+				 <div class="col-md-4 cate-grid grid">
+					<figure>
+						<img src="images/gen2.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen2.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+				 <div class="col-md-4 cate-grid grid">
+					 <figure>
+						<img src="images/gen1.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen1.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+
+
+				 <div class="col-md-4 cate-grid grid">
+					 <figure>
+						<img src="images/gen3.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen3.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+
+				 <div class="col-md-4 cate-grid grid">
+					<figure>
+						<img src="images/gen4.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen4.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+
+				 <div class="col-md-4 cate-grid grid">
+					<figure>
+						<img src="images/gen5.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen5.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+
+				 <div class="col-md-4 cate-grid grid">
+					 <figure>
+						<img src="images/gen6.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen6.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+				 				 <div class="col-md-4 cate-grid grid">
+					<figure>
+						<img src="images/gen1.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen1.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+				 <div class="col-md-4 cate-grid grid">
+					 <figure>
+						<img src="images/gen3.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen3.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+
+
+				 <div class="col-md-4 cate-grid grid">
+					 <figure>
+						<img src="images/gen2.jpg" alt="">
+						<figcaption>
+							<h3>Blogger</h3>
+							<span>Accusantium Dolor</span>
+							<a class="example-image-link" href="images/gen2.jpg" data-lightbox="example-1" data-title="Interior Design">VIEW</a>
+						</figcaption>
+					</figure>
+				 </div>
+ <script src="js/lightbox.js"></script>
+			 <div class="clearfix"></div>
+		 </div>
+	 </div>
+     <div class="clearfix"> </div>
+	<!--/footer-->
+	     <div class="footer">
+				 <div class="footer-top">
+				    <div class="col-md-4 footer-grid">
+					     <h4>Lorem sadipscing </h4>
+				          <ul class="bottom">
+							 <li>Consetetur sadipscing elitr</li>
+							 <li>Magna aliquyam eratsed diam</li>
+						 </ul>
+				    </div>
+					  <div class="col-md-4 footer-grid">
+					     <h4>Message Us Now</h4>
+				            <ul class="bottom">
+						     <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+							 <li><i class="glyphicon glyphicon-envelope"></i><a href="mailto:info@example.com">mail@example.com</a></li>
+						   </ul>
+				    </div>
+					<div class="col-md-4 footer-grid">
+					     <h4>Address Location</h4>
+				           <ul class="bottom">
+						     <li><i class="glyphicon glyphicon-map-marker"></i>2901 Glassgow Road, WA 98122-1090 </li>
+							 <li><i class="glyphicon glyphicon-earphone"></i>phone: (888) 123-456-7899 </li>
+						   </ul>
+				    </div>
+					<div class="clearfix"> </div>
+				 </div>
+	         </div>
+		<div class="copy">
+		    <p>&copy; 2016 Blogger. All Rights Reserved | Design by <a href="http://w3layouts.com/">W3layouts</a> </p>
+		</div>
+	 <div class="clearfix"> </div>
+	</div>
+	<div class="clearfix"> </div>
+</div>	
+		<!--//footer-->
+			<!--start-smooth-scrolling-->
+						<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+								</script>
+		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
