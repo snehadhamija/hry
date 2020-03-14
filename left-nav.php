514f5034cacc52bc1077bfaca720d6a257fdf338
Manisha7866 <61372903+Manisha7866@users.noreply.github.com>
2020-03-14 14:55:16 +0530

    Add files via upload

--- left-nav.php
+++ left-nav.php
@@ -3,17 +3,14 @@
 $response = api::call();
 class Api{
 public function call(){
-    $url = 'http://localhost:8090/haryana';
-    $collection_name = 'menu';
-    $request_url = $url . '/' . $collection_name;
-    $curl = curl_init($request_url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json'
-    ]);
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $response = json_decode($response,TRUE);
+$url = 'http://localhost:8090/haryana';
+$collection_name = 'menu';
+$request_url = $url . '/' . $collection_name;
+$curl = curl_init($request_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+curl_close($curl);
+$response = json_decode($response,TRUE);
    
 ?>
 
@@ -37,156 +34,65 @@ public function call(){
         <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
         <ul id="respMenu" class="ace-responsive-menu cl-effect-16" data-menu-style="vertical">
         <?php 
-		 
+		 // get menu name
 		 foreach($response as $key => $value) 
         {
-			echo "<li><a href='javascript:;'>";
+			echo "<li><a href='#'>";
             echo '<span class=title>' . $value['menu'] . '</span>';
-            echo '</a></li>';
-        }    
-    return $response;
-    }  
-
-function is_iterable($var)
-{
-    return $var !== null 
-        && (is_array($var) 
-            || $var instanceof Traversable 
-            || $var instanceof Iterator 
-            || $var instanceof IteratorAggregate
-            );
-}
-}
-function convert_to_array($response){
-    if(!is_array($response)){
-        $response = (array)$response;
-    }
-return $response;
-}
-?>
- 
-            <li>
-                <a href="javascript:;">
-                    <i class="fa fa-cube" aria-hidden="true"></i>
-                    <span class="title">About Us</span>
-			<span class="arrow"></span> 
-                </a>
-                <!-- Level Two-->
-                <ul>
-                    <li>
-                        <a href="#">Sub Item One</a>
-                    </li>
-                    <li>
-                        <a href="#">Sub Item Two</a>
-                    </li>
-                    <li>
-                        <a href="#">Sub Item Three</a>
-                    </li>
-                    <li>
-                        <a href="#">Sub Item Four</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript:;">
-                    <i class="fa fa-crop" aria-hidden="true"></i>
-                    <span class="title">4 Level Menu</span>
-			<span class="arrow"></span> 
-                </a>
-                <!-- Level Two-->
-                <ul>
-                    <li>
-                        <a href="javascript:;">
-                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                            Sub Item One						
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-database" aria-hidden="true"></i>
-                            Sub Item Two
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:;">
-                            <i class="fa fa-amazon" aria-hidden="true"></i>
-                            Sub Item Three							
-                        </a>
-                        <!-- Level Three-->
-                        <ul>
-                            <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i>Sub Item Link 1</a></li>
-                            <li>
-                                <a href="javascript:;">
-                                    <i class="fa fa-diamond" aria-hidden="true"></i>Sub Item Link 2</a>
-                                <!-- Level Four-->
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-trash" aria-hidden="true"></i>Sub Item Link 1</a></li>
-                                    <li><a href="#"><i class="fa fa-dashcube" aria-hidden="true"></i>Sub Item Link 2</a></li>
-                                    <li><a href="#"><i class="fa fa-dropbox" aria-hidden="true"></i>Sub Item Link 3</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i>Sub Item Link 3</a></li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-database" aria-hidden="true"></i>
-                            Sub Item Four
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="" href="javascript:;">
-                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                    <span class="title">Services</span>
-			<span class="arrow"></span> 
-                </a>
-                <ul>
-                    <li>
-                        <a href="#">Sub Item One
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:;">Sub Item Two							
-                        </a>
-                        <ul>
-                            <li><a href="#">Sub Item Link 1</a></li>
-                            <li><a href="#">Sub Item Link 2</a></li>
-                            <li><a href="#">Sub Item Link 3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="javascript:;">Sub Item Three							
-                        </a>
-                        <ul>
-                            <li><a href="#">Sub Item Link 1</a></li>
-                            <li><a href="#">Sub Item Link 1</a></li>
-                            <li><a href="#">Sub Item Link 1</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Sub Item Four
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:;">
-                    <i class="fa fa-heart" aria-hidden="true"></i>
-                    <span class="title">Products</span>
-                </a>
-            </li>
-            <li class="last ">
-                <a href="javascript:;">
-                    <i class="fa fa-envelope" aria-hidden="true"></i>
-                    <span class="title">Contact Us</span>
-                </a>
-            </li>
+            echo '</a>';
+			// get menu category
+			$id = $value['id'];
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'http://localhost:8090/haryana/menuCategory');
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+			curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			$result = curl_exec($ch);
+			$result_obj = json_decode($result, TRUE);
+			echo "<ul>";
+			foreach ($result_obj as $key => $obj) 
+			{
+     			if($obj['menu']['id']==$id)
+	 			{
+					echo "<li>
+                      <a href='#'>".$obj['category']."</a>";
+                 
+						// get menu services
+						
+						$ch1 = curl_init();
+						curl_setopt($ch1, CURLOPT_URL, 'http://localhost:8090/haryana/menuCatService');
+						curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+						curl_setopt($ch1, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+						curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+						$result1 = curl_exec($ch1);
+						$result_obj1 = json_decode($result1, TRUE);
+						 echo "<ul>";
+						foreach ($result_obj1 as $key => $obj1) 
+						{
+							if($obj1['menuCategory']['id']==$obj['id'])
+							{										
+							echo "<li>
+                                <a href='javascript:;'>
+                                    <i class='fa fa-diamond' aria-hidden='true'></i>".$obj1['service']."</a></li>";
+							}	
+						}
+					echo "</ul>";
+	 			}
+				echo "</li>";
+			}
+ 			echo "</ul></li>";
+        }
+		?>
         </ul>
     </nav>
+<?php   
+    }  
+
+}
+?>
+        
         <!-- script-for-nav -->
         <script>
             $( "span.menu" ).click(function() {
