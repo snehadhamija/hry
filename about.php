c52de91dd0d541fe96de30949380b106606a97aa
snehadhamija <61445355+snehadhamija@users.noreply.github.com>
2020-03-23 16:33:25 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -13,15 +13,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
      <?php include("top-nav.php"); ?>
 		
 	<div class="full">
-    <div class="col-md-3 top-nav">
+    <div class="col-md-4 top-nav">
 			<?php include("left-nav.php"); ?>
 		<!--banner-section-->
-	<div class="col-md-9 main">
+	<div class="col-md-8 main">
 		<?php include("banner.php"); ?>
 			<!--//banner-section-->
 			 <div class="b-bottom"> 
-			      <h5 class="top"><a href="single.php">What turn out consetetur sadipscing elit</a></h5>
-			      <p>On Aug 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+			      <h5 class="top"><a href="single.php"></a></h5>
+			      <p></p>
 				</div>
 			<!--/top-news-->
 			  <div class="top-news">
@@ -44,18 +44,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						</div>
 				</div>
 				 <div class="top-inner second">
-					<div class="col-md-6 top-text">
-						 <a href="single.php"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
-						    <h5 class="top"><a href="single.php">Consetetur sadipscing elit</a></h5>
-							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
-						    <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
-					 </div>
-					<div class="col-md-6 top-text two">
-						 <a href="single.php"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
-						    <h5 class="top"><a href="single.php">Consetetur sadipscing elit</a></h5>
-							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
-						    <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
-					 </div>
 					 <div class="clearfix"> </div>
 				 </div>
 	            </div>
--- news.php
+++ news.php
@@ -1,14 +1,11 @@
 </div>
 <div class="banner-right-text">
-			 <h3 class="tittle">News  <i class="glyphicon glyphicon-facetime-video"></i></h3>
+			 <h3 class="tittle">Consultant  <i class="glyphicon glyphicon-facetime-video"></i></h3>
 			<!--/general-news-->
 			 <div class="general-news">
 				<div class="general-inner">
 					<div class="general-text">
-						 <a href="single.php"><img src="images/gen1.jpg" class="img-responsive" alt=""></a>
-						    <h5 class="top"><a href="single.php">Consetetur sadipscing elit</a></h5>
-							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
-						    <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+						    <h5 class="top"><a href="#">Services</a></h5>
 					 </div>
 					 <div class="edit-pics">
 							      <div class="editor-pics">
@@ -17,8 +14,7 @@
 
 										   </div>
 											<div class="col-md-9 item-details">
-												<h5 class="inner two"><a href="single.php">New iPad App to simulate your Guitar</a></h5>
-												 <div class="td-post-date two"><i class="glyphicon glyphicon-time"></i>Feb 22, 2015 <a href="#"><i class="glyphicon glyphicon-comment"></i>0 </a></div>
+												<h5 class="inner two"><a href="#">Find New Machinery</a></h5>
 											 </div>
 											<div class="clearfix"></div>
 										</div>
@@ -28,8 +24,7 @@
 
 										   </div>
 											<div class="col-md-9 item-details">
-												<h5 class="inner two"><a href="single.php">New iPad App to simulate your Guitar</a></h5>
-												 <div class="td-post-date two"><i class="glyphicon glyphicon-time"></i>Feb 22, 2015 <a href="#"><i class="glyphicon glyphicon-comment"></i>0 </a></div>
+												<h5 class="inner two"><a href="#">Find Old Machinery</a></h5>
 											 </div>
 											<div class="clearfix"></div>
 										</div>
@@ -39,8 +34,7 @@
 
 										   </div>
 											<div class="col-md-9 item-details">
-												<h5 class="inner two"><a href="single.php">New iPad App to simulate your Guitar</a></h5>
-												 <div class="td-post-date two"><i class="glyphicon glyphicon-time"></i>Feb 22, 2015 <a href="#"><i class="glyphicon glyphicon-comment"></i>0 </a></div>
+												<h5 class="inner two"><a href="#">Find Employer</a></h5>
 											 </div>
 											<div class="clearfix"></div>
 										</div>
@@ -50,26 +44,22 @@
 
 										   </div>
 											<div class="col-md-9 item-details">
-												<h5 class="inner two"><a href="single.php">New iPad App to simulate your Guitar</a></h5>
-												 <div class="td-post-date two"><i class="glyphicon glyphicon-time"></i>Feb 22, 2015 <a href="#"><i class="glyphicon glyphicon-comment"></i>0 </a></div>
+												<h5 class="inner two"><a href="#">Find Employee </a></h5>
 											 </div>
 											<div class="clearfix"></div>
 										</div>
 									</div>
 								<div class="media">	
-								 <h3 class="tittle media">Media <i class="glyphicon glyphicon-floppy-disk"></i></h3>
+								 <h3 class="tittle media">Ads <i class="glyphicon glyphicon-floppy-disk"></i></h3>
 								  <div class="general-text two">
-									 <a href="single.php"><img src="images/gen3.jpg" class="img-responsive" alt=""></a>
-										<h5 class="top"><a href="single.php">Consetetur sadipscing elit</a></h5>
-										<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
-										<p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+									 <a href="#"><img src="images/gen3.jpg" class="img-responsive" alt=""></a>
+										<h5 class="top"><a href="#">Computer Plus Tally Academy</a></h5>
+										<p></p>
+										
 								  </div>
 					         </div>
 					    <div class="general-text two">
-						    <a href="single.php"><img src="images/gen2.jpg" class="img-responsive" alt=""></a>
-						    <h5 class="top"><a href="single.php">Consetetur sadipscing elit</a></h5>
-							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
-						    <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.php"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+						    
 					    </div>
 				 </div>
 			</div>	
