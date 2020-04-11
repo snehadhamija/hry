b3cab780cfcd5d3caf22d2a61ca6df01b2290251
snehadhamija <61445355+snehadhamija@users.noreply.github.com>
2020-04-11 12:39:14 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,5 +1,72 @@
 
-    <script src="js/ace-responsive-menu.js" type="text/javascript"></script>
+<link href="css/footer.css" rel='stylesheet' type='text/css' media="all">
+<footer class="buttom-footer py-lg-4 py-md-3 py-sm-3 py-3">
+         <section>
+            <div class="container py-lg-5 py-md-5 py-sm-4 py-4">
+               <div class="">
+                  <div class="row footer-agile-grids ">
+                     <div class="col-lg-4 col-md-4 col-sm-12 wthree-left-right">
+                        <h4 class="pb-lg-3 pb-3">Follow Us</h4>
+                        <div class="footer-icons">
+                           <ul style="margin:0px;padding:0px;">
+                              <li class="mb-2">
+                                 <a href="#"><span class="fab fa-facebook-f"></span></a>
+                                 <h6 class="ml-3 text-left">Facebook</h6>
+                              </li>
+                              <li class="mb-2">
+                                 <a href="#"><span class="fas fa-envelope"></span></a>
+                                 <h6 class="ml-3 text-left">Gmail</h6>
+                              </li>
+                              <li class="mb-2">
+                                 <a href="#"><span class="fab fa-twitter"></span></a>
+                                 <h6 class="ml-3 text-left">Twitter</h6>
+                              </li>
+                              
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="col-lg-4 col-md-4 col-sm-12 wthree-left-right">
+                        <h4 class="pb-lg-3 pb-3">&nbsp;    Links</h4>
+                        <nav class="border-line">
+                           <ul class="nav flex-column" id="foot">
+                              <li class="nav-item active">
+                                 <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                              </li>
+                              <li class="nav-item">
+                                 <a href="about.html" class="nav-link">About</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a href="contact.html" class="nav-link">Contact</a>
+                              </li>
+                           </ul>
+                        </nav>
+                     </div>
+                     <div class="wthree-left-right col-lg-4 col-md-4 col-sm-12">
+                        <h4 class="pb-lg-3 pb-3">Get In Touch</h4>
+                        <div class="wls-hours-list">
+                           <ul style="margin:0px;padding:0px;">
+                              <li class="d-flex">Contact:<span class="time ml-auto">01666-232543, 234565</span></li>
+                              <li class="d-flex">E-mail <span class="time ml-auto">kboffice.india@gmail.com</span></li>
+                              <li class="d-flex">Working Hours  <span class="time ml-auto">9am - 7pm</span></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!--<div class="copy-agile-right text-center">
+            <p>© 2018 Molten. All Rights Reserved | Design by <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a></p>
+         </div>-->
+      </footer>
+  <div class="clearfix"> </div>
+<div class="copy">
+		    <p>&copy; 2020 HaryanaIndustries.com | All Rights Reserved | Developed by <a href="#">DGLok IT Solutions Pvt. Ltd.</a></p>
+</div>
+<div class="clearfix"> </div>
+</div>
+</div>
+<script src="js/ace-responsive-menu.js" type="text/javascript"></script>
 	
 	<!--Plugin Initialization-->
     <script type="text/javascript">
