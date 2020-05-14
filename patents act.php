28c0b85c15709c50f60a9b87b94e2d23c64c05c6
snehadhamija <61445355+snehadhamija@users.noreply.github.com>
2020-05-14 21:07:24 +0530

    Add files via upload

--- patents act.php
+++ patents act.php
@@ -30,14 +30,14 @@ tr{
     <th width="50%" ><span> Title</span></th>
 	<th width="20%" ><span>   Download </span>   </th>
   </tr>
-   <tr><td>1</td><td>The Patents (Amendment) Act, 2002 (38 of 2002)</td><td><a href="ipindia\patent\act\1.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (619 KB)</a></td></tr>
-<tr><td>2</td><td>The Patents Act, 1970 ( incorporating all amendments till 23-06-2017)</td><td><a href="ipindia\patent\act\2.html" target="_blank">Html Page (928 KB)</a></td></tr>
-<tr><td>3</td><td>The Patents Act, 1970 ( incorporating all amendments till 23-06-2017)</td><td><a href="ipindia\patent\act\3.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (47 KB)</a></td></tr>
-<tr><td>4</td><td>The Patents (Amendment) Act 2005</td><td><a href="ipindia\patent\act\4.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (51 KB)</a></td></tr>
-<tr><td>5</td><td>The Patents (Amendment) Act - 26 March 1999</td><td><a href="ipindia\patent\act\5.PDF" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (45 KB)</a></td></tr>
-<tr><td>6</td><td>The Patents (Amendment) Act - 25 June 2002</td><td><a href="ipindia\patent\act\6.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (1.06 MB)</a></td></tr>
-<tr><td>7</td><td>The Patents Act 1970</td><td><a href="ipindia\patent\act\7.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (416 KB)</a></td></tr>
-<tr><td>8</td><td>The Patents Act 1970 (incorporating all amendments till 11-03-2015)</td><td><a href="ipindia\patent\act\8.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (124 KB)</a></td></tr>
+   <tr><td>1</td><td>The Patents (Amendment) Act, 2002 (38 of 2002)</td><td><a href="ipindia\patents\act\1.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (619 KB)</a></td></tr>
+<tr><td>2</td><td>The Patents Act, 1970 ( incorporating all amendments till 23-06-2017)</td><td><a href="ipindia\patents\act\2.html" target="_blank">Html Page (928 KB)</a></td></tr>
+<tr><td>3</td><td>The Patents Act, 1970 ( incorporating all amendments till 23-06-2017)</td><td><a href="ipindia\patents\act\3.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (47 KB)</a></td></tr>
+<tr><td>4</td><td>The Patents (Amendment) Act 2005</td><td><a href="ipindia\patents\act\4.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (51 KB)</a></td></tr>
+<tr><td>5</td><td>The Patents (Amendment) Act - 26 March 1999</td><td><a href="ipindia\patents\act\5.PDF" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (45 KB)</a></td></tr>
+<tr><td>6</td><td>The Patents (Amendment) Act - 25 June 2002</td><td><a href="ipindia\patents\act\6.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (1.06 MB)</a></td></tr>
+<tr><td>7</td><td>The Patents Act 1970</td><td><a href="ipindia\patents\act\7.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (416 KB)</a></td></tr>
+<tr><td>8</td><td>The Patents Act 1970 (incorporating all amendments till 11-03-2015)</td><td><a href="ipindia\patents\act\8.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/> (124 KB)</a></td></tr>
 
    </table>
 <div class="clearfix"> </div>
--- patents rule.php
+++ patents rule.php
@@ -33,19 +33,19 @@ tr{
   
   
 <tr><td>1</td><td>Draft Patents (2nd Amendment) Rules, 2019- published for Objections or suggestions</td><td><a href="ipindia\patent\rules\1.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(320 KB)</a></td></tr>
-<tr><td>2</td><td>Corrigendum in respect of Patents (Amendment) Rules, 2019 notified vide G.S.R. 663  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; (E) dated the 17th September, 2019</td><td><a href="ipindia\patent\rules\2.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(928 KB)</a></td></tr>
-<tr><td>3</td><td>Patents Amendment Rules, 2019</td><td><a href="ipindia\patent\rules\3.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(452 KB)</a></td></tr>
-<tr><td>4</td><td>The Patents Rules, 2003 (incorporating all amendments till 01-12-2017)</td><td><a href="ipindia\patent\rules\4.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(4.71 MB)</a></td></tr>
-<tr><td>5</td><td>The Patents Rules, 2003 (incorporating all amendments till 01-12-2017)</td><td><a href="ipindia\patent\rules\5.html" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>Html Page(278 KB)</a></td></tr>
-<tr><td>6</td><td>Patent (Amendment) Rules 2017</td><td><a href="ipindia\patent\rules\6.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(106 KB)</a></td></tr>
-<tr><td>7</td><td>Patents (Amendment) Rules 2016</td><td><a href="ipindia\patent\rules\7.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(528 KB)</a></td></tr>
-<tr><td>8</td><td>Draft Patent (Amendment) Rules, 2015 are published for public comments</td><td><a href="ipindia\patent\rules\8.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.34 MB)</a></td></tr>
-<tr><td>9</td><td>Patents (Amendment) Rules, 2014</td><td><a href="ipindia\patent\rules\9.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(5.53 MB)</a></td></tr>
-<tr><td>10</td><td>Gazette Notification of Patent (Amendements) Rules, 2013</td><td><a href="ipindia\patent\rules\10.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(414 KB)</a></td></tr>
-<tr><td>11</td><td>The Patent (Amendment) Rules, 2012</td><td><a href="ipindia\patent\rules\11.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(248 KB)</a></td></tr>
-<tr><td>12</td><td>The Patents (Amendment) Rules 2006, 05-05-2006 SO No. 657 (E)</td><td><a href="ipindia\patent\rules\12.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(529 KB)</a></td></tr>
-<tr><td>13</td><td>The Patents (Amendment) Rules 2005, 28-12-2004, SO No. 1418 (E)</td><td><a href="ipindia\patent\rules\13.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.12 MB)</a></td></tr>
-<tr><td>14</td><td>The Patents Rules, 2003</td><td><a href="ipindia\patent\rules\14.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.97 MB)</a></td></tr>
+<tr><td>2</td><td>Corrigendum in respect of Patents (Amendment) Rules, 2019 notified vide G.S.R. 663  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; (E) dated the 17th September, 2019</td><td><a href="ipindia\patents\rules\2.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(928 KB)</a></td></tr>
+<tr><td>3</td><td>Patents Amendment Rules, 2019</td><td><a href="ipindia\patents\rules\3.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(452 KB)</a></td></tr>
+<tr><td>4</td><td>The Patents Rules, 2003 (incorporating all amendments till 01-12-2017)</td><td><a href="ipindia\patents\rules\4.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(4.71 MB)</a></td></tr>
+<tr><td>5</td><td>The Patents Rules, 2003 (incorporating all amendments till 01-12-2017)</td><td><a href="ipindia\patents\rules\5.html" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>Html Page(278 KB)</a></td></tr>
+<tr><td>6</td><td>Patent (Amendment) Rules 2017</td><td><a href="ipindia\patents\rules\6.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(106 KB)</a></td></tr>
+<tr><td>7</td><td>Patents (Amendment) Rules 2016</td><td><a href="ipindia\patents\rules\7.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(528 KB)</a></td></tr>
+<tr><td>8</td><td>Draft Patent (Amendment) Rules, 2015 are published for public comments</td><td><a href="ipindia\patents\rules\8.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.34 MB)</a></td></tr>
+<tr><td>9</td><td>Patents (Amendment) Rules, 2014</td><td><a href="ipindia\patents\rules\9.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(5.53 MB)</a></td></tr>
+<tr><td>10</td><td>Gazette Notification of Patent (Amendements) Rules, 2013</td><td><a href="ipindia\patents\rules\10.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(414 KB)</a></td></tr>
+<tr><td>11</td><td>The Patent (Amendment) Rules, 2012</td><td><a href="ipindia\patents\rules\11.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(248 KB)</a></td></tr>
+<tr><td>12</td><td>The Patents (Amendment) Rules 2006, 05-05-2006 SO No. 657 (E)</td><td><a href="ipindia\patents\rules\12.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(529 KB)</a></td></tr>
+<tr><td>13</td><td>The Patents (Amendment) Rules 2005, 28-12-2004, SO No. 1418 (E)</td><td><a href="ipindia\patents\rules\13.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.12 MB)</a></td></tr>
+<tr><td>14</td><td>The Patents Rules, 2003</td><td><a href="ipindia\patents\rules\14.pdf" target="_blank"><img class="pdfIcon" src="images/file-pdf.jpg"  width="20" height="20"/>(1.97 MB)</a></td></tr>
 
 
    </table>
